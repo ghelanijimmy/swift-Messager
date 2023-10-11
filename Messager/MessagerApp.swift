f56d438c7e3139797b86910a2394707e9f7faf83
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-11 16:40:00 -0400

    Initial Commit

---
+++ Messager/MessagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagerApp.swift
+//  Messager
+//
+//  Created by Jimmy Ghelani on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct MessagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
